8736a6a33147de370eeb84bf081fb1cd7a4ae02a
b2un0 <bw@z-index.net>
2024-04-22 14:18:20 +0200

    build container by version and only if a new version exists

--- getbedrockserver.sh
+++ getbedrockserver.sh
@@ -3,10 +3,8 @@
 MC_DOWNLOAD_PAGE="https://www.minecraft.net/en-us/download/server/bedrock"
 CURL_USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15"
 
-#MC_DOWNLOAD_PAGE_CONTENT=$(curl -s -L -A "${CURL_USER_AGENT}" "${MC_DOWNLOAD_PAGE}")
-MC_DOWNLOAD_PAGE_CONTENT=$(cat download.html)
+curl -s -L -A "${CURL_USER_AGENT}" -o download.html "${MC_DOWNLOAD_PAGE}"
 
-echo "${MC_DOWNLOAD_PAGE_CONTENT}" | grep "linux/bedrock-server-" | egrep -o 'https?://[^" ]+'
+cat download.html | grep "linux/bedrock-server-" | egrep -o 'https?://[^" ]+'
 
-echo "${MC_DOWNLOAD_PAGE_CONTENT}" | grep "linux/bedrock-server-" | egrep -o 'https?://[^" ]+' | xargs basename -s ".zip" | sed -En 's#bedrock-server-([0-9.]+)#\1#p'
-
+cat download.html | grep "linux/bedrock-server-" | egrep -o 'https?://[^" ]+' | xargs basename -s ".zip" | sed -En 's#bedrock-server-([0-9.]+)#\1#p'
